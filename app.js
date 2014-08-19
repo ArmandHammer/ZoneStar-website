2e4eb351fade056d0c7e2bb746b26cbf9580e4dd
Armand Obreja <alex.1990.01@gmail.com>
2014-08-19 16:08:22 -0400

    init

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var oneDay = 86400000;
+var userCount = 0;
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 var port = Number(process.env.PORT || 8080);
 app.listen(port, function(){
+	userCount++;
 	console.log("listening on port " + port);
 });
